4b0184ac07ed2a7b837b5826698bc2967533055f
roopeaaltonen <roope.aaltonen@metropolia.fi>
2023-09-05 23:02:08 +0300

    kissa

--- 6.6..py
+++ 6.6..py
@@ -26,15 +26,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-def tervehdi():
-    print("Moi!")
-    tervehdi2()
-    return
-
-def tervehdi2 ():
-    print("MOROO!")
-
-print("Päivä alkaa tervehdyksellä.")
-tervehdi()
-print("Sitten siirrytään muihin asioihin.")
