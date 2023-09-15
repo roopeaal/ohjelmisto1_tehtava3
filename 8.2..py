47e49c04cc095a350264b21f59a7b49288278455
roopeaaltonen <roope.aaltonen@metropolia.fi>
2023-09-15 09:17:13 +0300

    kissa

--- 8.2..py
+++ 8.2..py
@@ -11,6 +11,16 @@ yhteys = mysql.connector.connect(
 
 maakoodi = input("Kerro haettava maakoodi (esimerkiksi FI): ")
 
+tyyppien_kutsumanimet = {
+    'balloonport': 'Ilmapallolaitureita',
+    'closed': 'Suljettuja kenttiä',
+    'heliport': 'Helikopterikenttiä',
+    'large_airport': 'Isoja lentokenttiä',
+    'medium_airport': 'Keskikokoisia lentokenttiä',
+    'seaplane_base': 'Vesilentokenttiä',
+    'small_airport': 'Pieniä lentokenttiä'
+}
+
 sql = "SELECT type, count(*) from airport"
 sql += " where iso_country ='" + maakoodi + "'group by type"
 
@@ -19,5 +29,8 @@ sql += " where iso_country ='" + maakoodi + "'group by type"
 kursori = yhteys.cursor()
 kursori.execute(sql)
 tulos = kursori.fetchall()
+
+
 for rivi in tulos:
-    print(f"{rivi[0]} on {rivi[1]} kappaletta.")
+    tyyppi = tyyppien_kutsumanimet.get(rivi[0], rivi[0])  # Haetaan suomenkielinen nimi sanakirjasta
+    print(f"{tyyppi} on {rivi[1]} kappaletta.")
